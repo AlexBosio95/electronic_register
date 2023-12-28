f5ef394bcfa0e761899924bc0ac2c0778fe0da17
Alex Bosio <101997868+AlexBosio95@users.noreply.github.com>
2023-12-28 14:39:40 +0000

    starter kit breeze

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -21,6 +21,11 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerPolicies();
+
+        Gate::define('access-route', function ($user) {
+            // Logica per determinare se l'utente può accedere alla route
+            return $user->isAllowedToAccessRoute(); 
+        });
     }
 }
