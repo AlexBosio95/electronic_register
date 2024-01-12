e8248d283925282b6bca808525d31a7d4d397bd3
AlexBosio95 <alex.bosio1995@gmail.com>
2024-01-12 17:18:33 +0100

    rimosso foreach che dava errore

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -2,9 +2,7 @@
     <div class="w-80 bg-[#1F2937] flex flex-col items-center pt-5 pb-2 space-y-7 h-[720px]">
         <!-- menu items -->
         <div class="w-full pr-3 flex flex-col gap-y-1 text-gray-500 fill-gray-500 text-sm cursor-pointer">
-            @foreach ($classes as $class)
-                <p>{{$class->name}}</p>
-            @endforeach
+        
             <div class="font-QuicksandMedium pl-4 text-gray-400/60 text-xs text-[11px] uppercase">Menu</div>
     
             <div class="w-full flex items-center gap-x-1.5 group select-none">
