eb7f574308b5c5ee184a3f42da1d43396514e9e4
AlexBosio95 <alex.bosio1995@gmail.com>
2024-01-14 17:48:08 +0100

    rimosso commenti inutili

--- app/Http/Controllers/AuthorizedUsers/DashboardController.php
+++ app/Http/Controllers/AuthorizedUsers/DashboardController.php
@@ -21,29 +21,21 @@ class DashboardController extends Controller
 
         $teacher = Teacher::where('user_id', $userId)->first();
 
-        // Verifica se l'insegnante è stato trovato
         if ($teacher) {
-            // Ottenere le classi associate all'insegnante
             $classes = $teacher->classes;
 
-            // Verifica se almeno una classe è associata all'insegnante
             if ($classes->isNotEmpty()) {
                 $selectedClassId = $request->input('selected_class');
 
-                // Verifica se è stata selezionata una classe specifica
                 if ($selectedClassId) {
-                    // Ottieni la classe selezionata
                     $selectedClass = $classes->where('id', $selectedClassId)->first();
 
-                    // Verifica se la classe selezionata è valida
                     if ($selectedClass) {
-                        // Ottieni gli studenti della classe selezionata
                         $students = $selectedClass->students;
                     } else {
                         return view('teacher.presents')->withErrors(['message' => 'Invalid selected class.']);
                     }
                 } else {
-                    // Supponendo che si vogliano gli studenti della prima classe trovata
                     $students = $classes->first()->students;
                 }
 
@@ -52,7 +44,6 @@ class DashboardController extends Controller
                 return view('teacher.presents')->withErrors(['message' => 'No classes found for the teacher.']);
             }
         } else {
-            // Gestire il caso in cui l'insegnante non è stato trovato
             return view('teacher.presents')->withErrors(['message' => 'Teacher not found.']);
         }
     }
