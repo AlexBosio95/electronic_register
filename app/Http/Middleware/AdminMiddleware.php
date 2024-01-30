04769812d11ec813a24d7cb1777411769aae8ab8
c <c>
2024-01-30 21:48:33 +0100

    iniziato modifiche al middleware

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -18,6 +18,9 @@ class AdminMiddleware
     {
         $user = Auth::user();
 
+        // TO DO Modificare il Middleware e controllare per prima cosa se il ruolo dell'utente corrente è nel config chiave visibility
+        // Visto che per certe rotte alcuni utenti hanno solo accesso alcuni metodi controllare anche questo e restituire 403 nei casi richiesti
+
         // Controlla se l'utente ha il ruolo 'admin'
         if ($user && $user->role === 'admin') {
             return $next($request);
