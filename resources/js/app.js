32a7e172bee532a78a0b71bc8a8c26bf3b7a12e4
c <c>
2024-04-06 16:29:16 +0200

    sistemazione menu classi

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,10 +6,12 @@ import axios from 'axios';
 window.axios = axios;
 import { createApp } from 'vue';
 import GradesComponent from './components/GradesComponent.vue';
+import MenuClassesComponent from './components/MenuClassesComponent.vue';
 
 const app = createApp({});
 
 app.component('grades-component', GradesComponent);
+app.component('menu-classes-component', MenuClassesComponent);
 
 app.mount('#app');
 
