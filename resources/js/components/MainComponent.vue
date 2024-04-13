1b0e7de03cf0a0380cbd067e553d8edd97931b53
c <c>
2024-04-13 17:34:47 +0200

    commentato il presents.blade e aggiunto lo spazio in cui mettere tutti i componenti

--- resources/js/components/MainComponent.vue
+++ resources/js/components/MainComponent.vue
@@ -1,14 +1,19 @@
 <template>
-    <div>
-        <div class="w-60 bg-[#1F2937] flex flex-col items-center pt-5 pb-2 space-y-7 h-[720px]">
-            <!-- menu items -->
-            <div class="w-full pr-3 flex flex-col gap-y-1 text-gray-500 fill-gray-500 text-sm cursor-pointer">
-                <menu-classes-component :classes="classes" :user_role="user_role" :page="page" :sections="sections"></menu-classes-component>
-                <div class="font-QuicksandMedium pl-4 text-gray-400/60 text-xs text-[11px] uppercase">Menu</div>
-                <menu-sections :classes="classes" :user_role="user_role" :page="page" :sections="sections"></menu-sections>
-            </div>
+   <div class="flex">
+    <div class="w-60 bg-[#1F2937] flex flex-col items-center pt-5 pb-2 space-y-7 h-[720px]">
+        <!-- menu items -->
+        <div class="w-full pr-3 flex flex-col gap-y-1 text-gray-500 fill-gray-500 text-sm cursor-pointer">
+            <menu-classes-component :classes="classes" :user_role="user_role" :page="page" :sections="sections"></menu-classes-component>
+            <div class="font-QuicksandMedium pl-4 text-gray-400/60 text-xs text-[11px] uppercase">Menu</div>
+            <menu-sections :classes="classes" :user_role="user_role" :page="page" :sections="sections"></menu-sections>
         </div>
     </div>
+    <!-- Componenti Vue -->
+    <div class="relative flex-grow bg-[#1F2937] overflow-scroll border-l border-red-500">
+        
+    </div>
+</div>
+
 </template>
 
 <script>
