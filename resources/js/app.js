d4cdfa8f05cea74262dbbab764ed8e522c0095a7
c <c>
2024-04-14 14:14:02 +0200

    componente presenze

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,8 @@ import { createApp } from 'vue';
 import GradesComponent from './components/GradesComponent.vue';
 import MenuClassesComponent from './components/MenuClassesComponent.vue';
 import MainComponent from './components/MainComponent.vue';
-import MenuSections from './components/MenuSections.vue'
+import MenuSections from './components/MenuSections.vue';
+import PresentsComponent from './components/PresentsComponent';
 
 const app = createApp({});
 
---
+++ resources/js/components/PresentsComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  
+    
+    
+</template>
+  
+<script>
+  
+  
+  export default {
+      props: {
+          classes: {
+              type: String,
+              required: true
+          },
+          user_role: {
+              type: String,
+              required: true
+          },
+          page: {
+              type: String,
+              required: true
+          }
+      },
+      data() {
+        return {
+          
+        };
+      },
+      methods: {
+        
+      }
+  };
+  
+  </script>
