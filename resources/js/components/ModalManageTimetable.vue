4e8974757d59778fcd8d7909d0754fc30b78b04a
c <c>
2024-09-30 21:30:03 +0200

    inserito il modale per modificare gli orari

---
+++ resources/js/components/ModalManageTimetable.vue
@@ -0,0 +1,40 @@
+<template>
+    
+</template>
+
+<script>
+
+
+
+export default {
+    props: {
+        students: {
+            type: Array,
+            default: () => []
+        },
+        current_class: {
+            type: Number
+        },
+        current_date: {
+            type: String
+        },
+        current_user: {
+            type: Number
+        }
+    },
+    data() {
+        return {
+            current_error: "",
+            type: "error",
+            popUpShow: false
+        };
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
